504ca03db6b5253ed7579f812e6ad0867a215930
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2023-11-01 14:17:20 +0100

    subir

--- T1/Array/utils/js/num4.js
+++ T1/Array/utils/js/num4.js
@@ -1,4 +1,20 @@
 let button4 = document.getElementById("for")
 button4.addEventListener("click",function () {
-    
+    let palo = ["C","D","R","P"];
+    let carta = []
+    for (let index = 0; index < palo.length; index++) {
+        for (let j = 1; j < 13; j++) {
+           carta.push(j+palo[index])
+        }
+    }
+    for (let i = 0; i < carta.length; i++) {
+        switch (i) {
+            case i.valueOf(10):
+                carta[i]=="J"//no sale
+                break;
+        }
+    }
+    console.log(carta);
+
+
 })
