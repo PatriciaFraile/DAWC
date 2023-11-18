e62ce536a33c6fbdee9c7bbdc9b05c4a71a1197c
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2023-11-18 01:15:29 +0100

    subir

---
+++ T1/Black_Jack/utils/js/baraja.js
@@ -0,0 +1,35 @@
+let baraja = []
+function crearBaraja() {
+    let palos = ["T", "C", "D", "P"];
+    for (let index = 0; index < palos.length; index++) {
+        for (let j = 1; j < 14; j++) {
+            let cartas = j;
+            switch (cartas) {
+                case 1:
+                    cartas = "A";
+                    break;
+                case 11:
+                    cartas = "J";
+                    break;
+                case 12:
+                    cartas = "Q";
+                    break;
+                case 13 :
+                    cartas = "K";
+                    break;
+            }
+            baraja.push(new carta(cartas+palos[index]))
+        }
+        
+    }
+    baraja = _.shuffle(baraja)
+    
+}
+function mostrarBaraja() {
+    setInterval(() => {
+        let carta = baraja.pop()
+        carta.mostrarDatos()
+    }, 2000);
+}   
+crearBaraja();
+mostrarBaraja();
--- T1/Black_Jack/utils/js/carta.js
+++ T1/Black_Jack/utils/js/carta.js
@@ -3,26 +3,31 @@ class carta{
     nombre;
     imagen;
     constructor(nombre){
-        this.valor = this.calcularValor();
         this.nombre = nombre;
-        this.imagen =  nombre + "png"
+        this.imagen =  nombre + ".png"
+        this.valor = this.calcularValor()
     }
-    calcularValor(){
-        let valor = this.nombre.substring(0,this.nombre.lenght-1);
+    calcularValor() {
+        let valor = this.nombre.substring(0, this.nombre.length - 1);
+    
         switch (valor) {
-            case "J":
-            case "Q":
-            case "K":
-                valor = 10
-                break;
-            default:
-                valor = Number(this.nombre.substring(0,this.nombre.lenght-1));
+          case "K":
+          case "J":
+          case "Q":
+            valor = 10;
+            break;
+          default:
+            valor = Number(this.nombre.substring(0, this.nombre.length - 1));
+            break;
         }
+    
         return valor;
-        }
+      }
+    
         mostrarDatos(){
-            console.log(`Nombre: ${this.nombre},
-            Valor: ${this.valor},
-            Imagen : ${this.imagen} `)
+            console.log(`Nombre: ${this.nombre} `)
+            console.log(`Imagen: ${this.imagen} `)
+            console.log(`Valor: ${this.valor} `)
+
         }
 }
