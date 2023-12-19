8d07d5ab515f130854bddd812a344aefe18f46d4
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2023-12-19 22:08:09 +0100

    subir

--- T2/Crypto/utils/js/main.js
+++ T2/Crypto/utils/js/main.js
@@ -1,11 +1,11 @@
 let fila = document.querySelector(".col-8 .row");
 let boton = document.querySelector("button");
 let select = document.querySelector("select");
+let fila2 = document.querySelector("#colum #fila #linea")
 
 boton.addEventListener("click", (e) => {
   resetearLista();
   pintarDatos(select.value);
-  mostrar(select.value);
 
 });
 
@@ -27,27 +27,31 @@ function pintarDatos(genero) {
       fila.innerHTML += `<div class="col">
         <div class="card" style="width: 18rem">
           <img src="${element.picture.medium}" class="card-img-top" alt="..." />
-          <div class="card-body">
+          <div class="card-body" id="carta">
             <h5 class="card-title">${element.name.first} ${element.name.last}</h5>
             <p class="card-text">${element.email}</p>
-            <a href="#" class="btn btn-primary">Ver detalle</a>
+            <a href="#" class="btn btn-primary" id="boton">Ver detalle</a>
           </div>
         </div>
       </div>`;
-      let boton = document.querySelector(".btn btn-primary");
-      boton.addEventListener('click',(e)=>{
-        getUsers(genero).then((data)=>{
-          data.results.forEach((element)=>{
-            console.log(element.name.first);
-          })
-        })
-    
-      })
+      let boton2 = document.querySelector("#boton")
+      let carta = document.querySelector("#carta")
+
+  boton2.addEventListener("click",()=>{
+    let carta2 = carta.textContent
+    let array = [carta2]
+    mostrarDatos(array)
+  })
+      
     });
-  });
+})
 }
-function mostrar(genero){
-  
+function mostrarDatos(info){
+  info.forEach((e)=>{
+    fila2.textContent += e
+    console.log(e);
+  })
+
   
 }
 
