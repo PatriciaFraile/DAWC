272a7d731cadf9901ca1a554c157178e43774a44
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2024-03-03 14:08:48 +0100

    update

---
+++ T3/react/concesionario/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Concesionario from './components/Concesionario';
+
+function App(){
+  return (
+    <Concesionario></Concesionario>
+  );
+}
+
+export default App;
