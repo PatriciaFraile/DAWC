90f7b62d05f1e592060b2e157f1585553875b7be
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2024-05-06 22:54:17 +0200

    update

---
+++ Repaso/repaso/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+  </div>
+</nav>
+  );
+}
+
+export default App;
